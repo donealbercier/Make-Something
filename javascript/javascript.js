8d75a5b6e865c2f9af7463222c7428f4456f48fc
Doneal Bercier <donealbercier@gmail.com>
2018-02-02 09:55:31 -0500

    Major Theme Change and Layout
    #Fixed Font-Errors#
    -Added Media Queries and Layout Changes
    +Removed starting animations
    +Expiremented with Clip-Paths
    -Added Instructions and Ingredients
    +made change divs
    +classes for editable sections for JavaScript
    -Made title Editable
    -Relegated Theme Changes to scss variables
    
    #Changed JavaScript libraries
    -Removed typed.js Css library

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,15 +1,12 @@
 $(document).ready(function(){
+    $( "#changeable" ).focus();
+  //  $("#changeable").html("Hello");
 // console.log("we've got this");
 // var webData = $.getJSON("http://localhost:8080/greeting");
 // $("#change").html("<h1>"+webData+"</h1>");
 // console.log(webData);
 // Can also be included with a regular script tag
-import Typed from 'typed.js';
 
-var options = {
-  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-  typeSpeed: 40
-}
 
-var typed = new Typed(".element", options)
+
 });
